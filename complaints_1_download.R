1ee29ff82f77196f2a85446f1f34f141005d63d7
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-02-02 21:10:51 -0500

    :tada: download HPD complaints(problems) files

---
+++ complaints_1_download.R
@@ -0,0 +1,48 @@
+library(tidyverse) 
+library(magrittr)
+library(stringr)
+library(rvest)
+library(here) 
+
+
+dir.create(here("data-raw"), showWarnings = FALSE)
+dir.create(here("data-raw", "documentation"), showWarnings = FALSE)
+dir.create(here("data-raw", "complaints"), showWarnings = FALSE)
+
+
+# Documentation Files -----------------------------------------------------
+
+download.file("http://www1.nyc.gov/assets/hpd/downloads/pdf/ComplaintsOpenDataDoc.zip", 
+              here("data-raw", "documentation", "ComplaintsOpenDataDoc.zip"), mode = "wb", quiet = TRUE)
+
+unzip(here("data-raw", "documentation", "ComplaintsOpenDataDoc.zip"), exdir = here("data-raw", "documentation"))
+
+# Delete xsd and zip files
+here("data-raw", "documentation") %>%
+  dir(pattern = "\\.(xsd|zip)$", full.names = TRUE) %>% 
+  file.remove()
+
+
+# Data Files --------------------------------------------------------------
+
+data_urls <- read_html("http://www1.nyc.gov/site/hpd/about/Complaints-open-data.page") %>% 
+  html_nodes(".about-description ul li a") %>% 
+  html_attr("href") %>% 
+  xml2::url_absolute("http://www1.nyc.gov/")
+
+download_complaints <- function(url) {
+  filename <- str_extract(url, "Complaints*\\d{8}")
+  
+  dir.create(here("data-raw", "complaints"), showWarnings = FALSE)
+  
+  download.file(url, here("data-raw", "complaints", str_c(filename, ".zip")), mode = "wb", quiet = TRUE)
+  
+  unzip(here("data-raw", "complaints", str_c(filename, ".zip")), exdir = here("data-raw", "complaints"))
+}
+
+walk(data_urls, download_complaints)
+
+# Delete xml and zip files
+here("data-raw", "complaints") %>%
+  dir(pattern = "\\.(xml|zip)$", full.names = TRUE) %>% 
+  file.remove()
