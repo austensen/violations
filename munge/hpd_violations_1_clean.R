bd92a32726fd32ea357b819c32539e4e0a69d5c4
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-03-26 18:35:13 -0400

    adjust bbl and tract violation counts, drop bock-level vars; read cols_only

--- munge/hpd_violations_1_clean.R
+++ munge/hpd_violations_1_clean.R
@@ -6,97 +6,96 @@ dir.create("data", showWarnings = FALSE)
 
 # Import Raw and Clean ----------------------------------------------------
 
-violation_cols <- cols(
-  ViolationID = col_integer(),
-  BuildingID = col_integer(),
-  RegistrationID = col_integer(),
+violation_cols <- cols_only(
+  # ViolationID = col_integer(),
+  # BuildingID = col_integer(),
+  # RegistrationID = col_integer(),
   BoroID = col_integer(),
-  Boro = col_character(),
-  HouseNumber = col_character(),
-  LowHouseNumber = col_character(),
-  HighHouseNumber = col_character(),
-  StreetName = col_character(),
-  StreetCode = col_integer(),
-  Zip = col_integer(),
+  # Boro = col_character(),
+  # HouseNumber = col_character(),
+  # LowHouseNumber = col_character(),
+  # HighHouseNumber = col_character(),
+  # StreetName = col_character(),
+  # StreetCode = col_integer(),
+  # Zip = col_integer(),
   Apartment = col_character(),
-  Story = col_character(),
+  # Story = col_character(),
   Block = col_integer(),
   Lot = col_integer(),
   Class = col_character(),
-  InspectionDate = col_date("%m/%d/%Y"),
-  ApprovedDate = col_date("%m/%d/%Y"),
-  OriginalCertifyByDate = col_date("%m/%d/%Y"),
-  OriginalCorrectByDate = col_date("%m/%d/%Y"),
-  NewCertifyByDate = col_date("%m/%d/%Y"),
-  NewCorrectByDate = col_date("%m/%d/%Y"),
-  CertifiedDate = col_date("%m/%d/%Y"),
-  OrderNumber = col_character(),
-  NOVID = col_integer(),
-  NOVDescription = col_character(),
-  NOVIssuedDate = col_date("%m/%d/%Y"),
-  CurrentStatusID = col_integer(),
-  CurrentStatus = col_character(),
-  CurrentStatusDate = col_date("%m/%d/%Y")
+  InspectionDate = col_date("%m/%d/%Y")
+  # ApprovedDate = col_date("%m/%d/%Y"),
+  # OriginalCertifyByDate = col_date("%m/%d/%Y"),
+  # OriginalCorrectByDate = col_date("%m/%d/%Y"),
+  # NewCertifyByDate = col_date("%m/%d/%Y"),
+  # NewCorrectByDate = col_date("%m/%d/%Y"),
+  # CertifiedDate = col_date("%m/%d/%Y"),
+  # OrderNumber = col_character(),
+  # NOVID = col_integer(),
+  # NOVDescription = col_character(),
+  # NOVIssuedDate = col_date("%m/%d/%Y"),
+  # CurrentStatusID = col_integer(),
+  # CurrentStatus = col_character(),
+  # CurrentStatusDate = col_date("%m/%d/%Y")
 )
 
 viol_raw <- read_csv("data-raw/hpd_violations/hpd_violations.csv", col_types = violation_cols)
 
-block_tract10_xwalk <- read_feather("data-raw/crosswalks/block_tract10_xwalk.feather")
+bbl_tract10_units <- read_feather("data/bbl_tract10_units.feather")
+
+# bbl viols = bldg + (apt / bbl_units)
+# tract viols = ((bldg * bbl_units) + apt ) / tract_units
 
 bbl_viol <- viol_raw %>% 
-  janitor::clean_names() %>% 
-  filter(lubridate::year(inspectiondate) %in% 2013:2016) %>% 
-  mutate(bbl = str_c(boroid, str_pad(block, 5, "left", "0"), str_pad(lot, 4, "left", "0")),
-         block = str_sub(bbl, 1, 6),
-         year = lubridate::year(inspectiondate),
-         scope = if_else(is.na(apartment), "bldg", "apt"),
-         class = if_else(class == "C", "ser", "oth")) %>% 
-  left_join(block_tract10_xwalk, by = "block") %>% 
-  group_by(tract10, block, bbl, year, class, scope) %>% 
-  summarise(violations = n()) %>% 
+  filter(lubridate::year(InspectionDate) %in% 2013:2016) %>% 
+  mutate(bbl = str_c(BoroID, str_pad(Block, 5, "left", "0"), str_pad(Lot, 4, "left", "0")),
+         year = lubridate::year(InspectionDate),
+         space = if_else(is.na(Apartment), "bldg", "apt"),
+         class = if_else(Class == "C", "ser", "oth")) %>% 
+  semi_join(bbl_tract10_units, by = "bbl") %>% 
+  group_by(bbl, year, class, space) %>% 
+  summarise(violations = n())
+
+
+adj_bbl_viol <- bbl_viol %>% 
+  inner_join(bbl_tract10_units, by = "bbl") %>% 
+  mutate(adj_bbl_viol = if_else(space == "apt", violations / res_units, as.double(violations))) %>% 
+  group_by(bbl, tract10, year, class) %>% 
+  summarise(adj_viol = sum(adj_bbl_viol)) %>% 
   mutate(geo = "bbl") %>% 
   ungroup
 
-block_viol <- bbl_viol %>% 
-  group_by(block, year, class, scope) %>% 
-  summarise(violations = n()) %>% 
-  mutate(geo = "blk") %>% 
-  ungroup
+adj_bbl_viol_wide <- adj_bbl_viol %>% 
+  mutate(geo_class_year = str_c("viol", geo, class, year, sep = "_")) %>% 
+  select(bbl, tract10, geo_class_year, adj_viol) %>% 
+  spread(geo_class_year, adj_viol, fill = 0)
 
-tract_viol <- bbl_viol %>% 
-  filter(!is.na(tract10)) %>% 
-  group_by(tract10, year, class, scope) %>% 
+adj_tract_viol <- bbl_viol %>% 
+  filter(class == "ser") %>% 
+  inner_join(bbl_tract10_units, by = "bbl") %>% 
+  group_by(tract10, tract_res_units, year, space) %>% 
+  mutate(violations = if_else(space == "bldg", violations * res_units, violations)) %>% 
   summarise(violations = n()) %>% 
+  mutate(adj_tract_viol = violations / tract_res_units) %>% 
+  group_by(tract10, year) %>% 
+  summarise(adj_viol = sum(adj_tract_viol)) %>% 
   mutate(geo = "trct") %>% 
   ungroup
+  
+adj_tract_viol_wide <- adj_tract_viol %>% 
+  mutate(geo_year = str_c("viol", geo, year, sep = "_")) %>% 
+  select(tract10, geo_year, adj_viol) %>% 
+  spread(geo_year, adj_viol, fill = 0)
 
-reshape_wide <- function(.data, geo_type) {
-  .data %>% 
-    mutate(geo_scope_class_year = str_c("viol", geo, scope, class, year, sep = "_")) %>% 
-    select_(.dots = c(geo_type, "geo_scope_class_year", "violations")) %>% 
-    spread(geo_scope_class_year, violations, fill = 0)
-}
-
-bbl_viol_wide <- reshape_wide(bbl_viol, "bbl")
-block_viol_wide <- reshape_wide(block_viol, "block")
-tract_viol_wide <- reshape_wide(tract_viol, "tract10")
-
-all_viol_wide <- bbl_viol_wide %>% 
-  mutate(block = str_sub(bbl, 1, 6)) %>% 
-  left_join(block_tract10_xwalk, by = "block") %>% 
-  left_join(block_viol_wide, by = "block") %>% 
-  left_join(tract_viol_wide, by = "tract10") %>% 
-  select(-block, -tract10)
+adj_all_viol_wide <- adj_bbl_viol_wide %>% 
+  left_join(adj_tract_viol_wide, by = "tract10") %>% 
+  select(-tract10)
 
 
-write_feather(all_viol_wide, "data/hpd_violations.feather")
+write_feather(adj_all_viol_wide, "data/hpd_violations.feather")
 
 # Make tract-level wide counts of violatiosn for mapping
-tract_viol_map <- tract_viol %>% 
-  group_by(tract10, year, class) %>% 
-  summarise(violations = sum(violations, na.rm = TRUE)) %>% 
-  unite(class_year, class, year) %>% 
-  spread(class_year, violations) %>% 
-  ungroup
+tract_viol_map <- adj_tract_viol_wide %>% 
+  mutate_at(vars(-tract10), funs(. * 1000))
 
 write_feather(tract_viol_map, "data/hpd_violations_map_data.feather")
