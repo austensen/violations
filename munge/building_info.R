d2152d8112fd0d5403e39116f295c5d76f9ec5bc
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-03-26 18:32:50 -0400

    filter bbls to 3+ unit rental, make bbl_tract_units

--- munge/building_info.R
+++ munge/building_info.R
@@ -7,7 +7,8 @@ dir.create("data", showWarnings = FALSE)
 rpad <- read_feather("data-raw/dof_rpad/rpad_15.feather")
 pluto <- read_feather("data-raw/dcp_pluto/pluto_16.feather")
 
-building_info_all <- full_join(rpad, pluto, by = "bbl", suffix = c("_rpad", "_pluto")) %>% 
+# consolidate pluto/rpad variables were overlap, fill out missing tract IDs
+building_info_filtered <- full_join(rpad, pluto, by = "bbl", suffix = c("_rpad", "_pluto")) %>% 
   mutate(block = str_sub(bbl, 1, 6),
          cd = if_else(is.na(cd_pluto), cd_rpad, cd_pluto),
          building_class = if_else(is.na(building_class_pluto), building_class_rpad, building_class_pluto),
@@ -23,44 +24,26 @@ building_info_all <- full_join(rpad, pluto, by = "bbl", suffix = c("_rpad", "_pl
   group_by(block) %>% 
   mutate(tract10 = max(tract10, na.rm = TRUE)) %>% 
   ungroup %>% 
-  select(-matches("rpad|pluto$"))
-  # warnings are just cases where tract10 is missing for all bbls in block, that's okay
-
-# Get counts of all res units by tract for maps
-tract_units <- building_info_all %>% 
+  select(-matches("rpad|pluto$")) %>% 
   filter( # remove vacant land, city-owned, single-family, coops, condos (imperfect, but better than nothing)
-    as.numeric(tax_class1) < 3,
+    res_units >= 3,
+    str_sub(tax_class, 1, 1) %in% 1:2,
     tax_class != "1B",
-    res_units > 0,
     !owner_type %in% c("C", "M", "O"),
     !building_class1 %in% c("A","Z","G","V"),
     !building_class %in% c("C6","C8","D0","D4","R0","R1","R2","R3","R4","R5","R6","R7","R8","R9")
-  ) %>% 
-  # remove one unit that would most likely be owner's 
-  mutate(res_units = if_else(as.numeric(tax_class1) == 1, res_units - 1, res_units)) %>% 
+  )
+  # warnings are just cases where tract10 is missing for all bbls in block, that's okay
+
+bbl_tract10_units <- building_info_filtered %>% 
+  select(bbl, tract10, res_units) %>% 
   group_by(tract10) %>% 
-  summarise(res_units = sum(res_units, na.rm = TRUE)) %>% 
+  mutate(tract_res_units = sum(res_units, na.rm = TRUE),
+         tract_res_units = if_else(is.na(tract10), NA_integer_, tract_res_units)) %>% 
   ungroup
 
+building_info <- building_info_filtered %>% 
+  select(-tract10, -block, -tax_class, -zoning, -building_class, -owner_type)
 
-# Sample for prediction modeling
-# restrict to only 3+ units, and remove vacant land, single-family, coops, condos 
-building_info <- building_info_all %>% 
-  filter(res_units >= 3,
-         !owner_type %in% c("C", "M", "O"),
-         !building_class1 %in% c("A","Z","G","V"),
-         !building_class %in% c("C6","C8","D0","D4","R0","R1","R2","R3","R4","R5","R6","R7","R8","R9")) %>% 
-  select(-tax_class, -zoning, -building_class, -owner_type)
-
-
-# Create block-tract xwalk since blocks always nest in tracts, and there are fewer missing tracts by block
-block_tract10_xwalk <- building_info_all %>% 
-  filter(!is.na(tract10)) %>% 
-  select(block, tract10) %>% 
-  group_by(block) %>% 
-  slice(1) %>% 
-  ungroup
-
-write_feather(tract_units, "data/tract10_res_units.feather")
 write_feather(building_info, "data/building_info.feather")
-write_feather(block_tract10_xwalk, "data-raw/crosswalks/block_tract10_xwalk.feather")
+write_feather(bbl_tract10_units, "data/bbl_tract10_units.feather")
