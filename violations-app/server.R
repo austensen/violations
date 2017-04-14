837b690da25fcfec1b38ef8d075757623802a58e
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-04-14 17:22:03 -0400

    add DT row selected popups

--- violations-app/server.R
+++ violations-app/server.R
@@ -20,7 +20,7 @@ function(input, output, session) {
     cd_df() %>% 
       leaflet() %>%
       addProviderTiles(providers$CartoDB.Positron) %>% 
-      addPolygons(fillColor = NULL) %>%
+      addPolygons(color = NULL, fillColor = NULL) %>%
       addLegend(pal = pal, values = 0:1, opacity = 1, title = NULL, position = "bottomright")
   })
   
@@ -42,11 +42,40 @@ function(input, output, session) {
                                                       bringToFront = TRUE))
   })
   
-  output$tbl <- DT::renderDataTable({
+  cd_tbl <- reactive({
     cd_df() %>% 
       tibble::as_tibble() %>% 
       rename_(.dots = setNames(models[[input$model]], "prediction")) %>% 
       select(bbl, prediction) %>% 
       arrange(desc(prediction))
   })
+  
+  output$tbl <- DT::renderDataTable(cd_tbl(), selection = "single")
+  
+  observe({
+    if (is.null(input$tbl_rows_selected)) {
+      leafletProxy("map") %>% clearPopups()
+      return()
+    }
+    
+    bbl_selected <- cd_tbl()[["bbl"]][[input$tbl_rows_selected]]
+    
+    bbl_df <- filter(cd_df(), bbl == as.character(bbl_selected))
+
+    bbl_bbox <- st_bbox(bbl_df)
+
+    bbl_lng <- mean(bbl_bbox[c("xmin", "xmax")])
+    bbl_lat <- mean(bbl_bbox[c("ymin", "ymax")])
+
+    leafletProxy("map") %>% 
+      clearPopups() %>%
+      addPopups(lng = bbl_lng, lat = bbl_lat, 
+                popup = paste("BBL:", bbl_df$bbl, "<br>",
+                              "Actual Violations:", bbl_df$true_16, "<br>",
+                              "Previous Year Violations:", bbl_df$past_viol, "<br>",
+                              "Logit Predictions:", round(bbl_df$logit, 2), "<br>",
+                              "Decision Tree Predictions:", round(bbl_df$tree, 2), "<br>",
+                              "Random Forest Predictions:", round(bbl_df$forest, 2), "<br>"))
+  })
+  
 }
