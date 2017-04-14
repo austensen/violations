4a42f5a7ce78e491cfcab736bf39a3298e317e20
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-04-14 15:48:44 -0400

    move model changes to observe call

--- violations-app/server.R
+++ violations-app/server.R
@@ -14,26 +14,39 @@ function(input, output, session) {
     filter(map_df, cd %in% cds[[input$cd]])
   })
   
+  
   output$map <- renderLeaflet({
+    
     cd_df() %>% 
-      rename_(.dots = setNames(models[[input$model]], "pred")) %>% 
       leaflet() %>%
       addProviderTiles(providers$CartoDB.Positron) %>% 
+      addPolygons(fillColor = NULL) %>%
+      addLegend(pal = pal, values = 0:1, opacity = 1, title = NULL, position = "bottomright")
+  })
+  
+  observe({
+    fill_var <- cd_df()[[models[[input$model]]]]
+    
+    leafletProxy("map", data = cd_df()) %>% 
+      clearShapes() %>% 
       addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
                   opacity = 1, fillOpacity = 1,
-                  fillColor = ~pal(pred),
-                  popup = ~bbl,
+                  fillColor = ~pal(fill_var),
+                  popup = paste("BBL:", cd_df()$bbl, "<br>",
+                                "Actual Violations:", cd_df()$true_16, "<br>",
+                                "Previous Year Violations:", cd_df()$past_viol, "<br>",
+                                "Logit Predictions:", round(cd_df()$logit, 2), "<br>",
+                                "Decision Tree Predictions:", round(cd_df()$tree, 2), "<br>",
+                                "Random Forest Predictions:", round(cd_df()$forest, 2), "<br>"),
                   highlightOptions = highlightOptions(color = "white", weight = 2,
-                                                      bringToFront = TRUE)) %>%
-      addLegend(pal = pal, values = ~pred, opacity = 1, title = NULL,
-                position = "bottomright")
+                                                      bringToFront = TRUE))
   })
   
   output$tbl <- DT::renderDataTable({
     cd_df() %>% 
       tibble::as_tibble() %>% 
-      rename_(.dots = setNames(models[[input$model]], "pred")) %>% 
-      select(bbl, pred) %>% 
-      arrange(desc(pred))
+      rename_(.dots = setNames(models[[input$model]], "prediction")) %>% 
+      select(bbl, prediction) %>% 
+      arrange(desc(prediction))
   })
 }
