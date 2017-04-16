f13fa2dbb80b37741432c37a61091a7c52e16d9b
Maxwell Austensen <maxwell.austensen@nyu.edu>
2017-04-16 16:31:17 -0400

    cut old violation adjustments in merge_all.R (updated zipped file), add unzip to analysis

--- munge/merge_all.R
+++ munge/merge_all.R
@@ -9,14 +9,6 @@ lit <- read_feather("data/hpd_litigation.feather")
 df <- building_info %>% 
   left_join(viol, by = "bbl") %>% 
   left_join(lit, by = "bbl") %>%
-  mutate_at(vars(matches("\\d{4}$")), funs(if_else(is.na(.), 0, .))) %>% 
-  mutate_at(vars(matches("^viol_bbl_apt")), funs(. / res_units)) %>% 
-  group_by(block) %>% # adjust block and tract level violation counts by appropirate denominators
-  mutate_at(vars(matches("^viol_blk_apt")), funs(. / sum(res_units, na.rm = TRUE))) %>%
-  mutate_at(vars(matches("^viol_blk_bldg")), funs(. / n())) %>%
-  group_by(tract10) %>% 
-  mutate_at(vars(matches("^viol_trct_apt")), funs(. / sum(res_units, na.rm = TRUE))) %>% 
-  mutate_at(vars(matches("^viol_trct_bldg")), funs(. / n())) %>%
   mutate_if(is.character, as.factor) %>% 
   ungroup
   
